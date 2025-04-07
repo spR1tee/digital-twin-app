ffd09860c12bf2ffdabcbbeb6a9f26504c6e3627
Eliot Nagy <nagyeliot@gmail.com>
2025-04-07 22:34:35 +0200

    remove unused methods

--- src/main/java/hu/digital_twin/service/SimulatorService.java
+++ src/main/java/hu/digital_twin/service/SimulatorService.java
@@ -2,15 +2,6 @@ package hu.digital_twin.service;
 
 import hu.digital_twin.Simulation;
 import hu.digital_twin.model.RequestData;
-import hu.digital_twin.model.VmData;
-import hu.mta.sztaki.lpds.cloud.simulator.iaas.PhysicalMachine;
-import hu.mta.sztaki.lpds.cloud.simulator.iaas.VMManager;
-import hu.mta.sztaki.lpds.cloud.simulator.iaas.VirtualMachine;
-import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.AlterableResourceConstraints;
-import hu.mta.sztaki.lpds.cloud.simulator.io.NetworkNode;
-import hu.mta.sztaki.lpds.cloud.simulator.io.Repository;
-import hu.mta.sztaki.lpds.cloud.simulator.io.VirtualAppliance;
-import hu.u_szeged.inf.fog.simulator.provider.Instance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -21,8 +12,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class SimulatorService {
@@ -32,21 +21,13 @@ public class SimulatorService {
     @Autowired
     private Simulation simulation;
 
-    private final List<VirtualAppliance> virtualAppliances = new ArrayList<>();
-    private final List<VirtualMachine> VMs = new ArrayList<>();
-    private final List<Instance> instances = new ArrayList<>();
-    Repository repo;
-    PhysicalMachine pm;
-
     public SimulatorService() {}
 
     public void handleRequest(RequestData requestData) {
         String request_type = requestData.getRequestType().toUpperCase();
         switch (request_type) {
             case "UPDATE":
-                simulation.do_alternative("down");
-                simulation.do_alternative("up");
-                //requestDataService.createRequestData(requestData);
+                requestDataService.createRequestData(requestData);
                 /*List<RequestData> query = requestDataService.getAllRequestData();
                 for(RequestData rd : query) {
                     System.out.println(rd.toString());
@@ -83,44 +64,6 @@ public class SimulatorService {
         }
     }
 
-    public void createVM(VmData vmData) throws VMManager.VMManagementException, NetworkNode.NetworkException {
-        createVirtualAppliance(vmData);
-        repo.registerObject(getVA(vmData.getName()));
-        VMs.add(pm.requestVM(getVA(vmData.getName()),
-                new AlterableResourceConstraints(vmData.getCpu(), vmData.getCoreProcessingPower(), vmData.getRam()), repo, 1)[0]);
-    }
-
-    public VirtualAppliance getVA(String id) {
-        for (VirtualAppliance va : virtualAppliances) {
-            if (va.id.equals(id)) {
-                return va;
-            }
-        }
-        System.err.println("VirtualAppliance not found.");
-        return null;
-    }
-
-    public VirtualMachine getVM(String id) {
-        for (VirtualMachine vm : VMs) {
-            if (vm.getVa().id.equals(id)) {
-                return vm;
-            }
-        }
-        System.err.println("VirtualMachine not found.");
-        return null;
-    }
-
-
-    public void createVirtualAppliance(VmData vmData) {
-        virtualAppliances.add(new VirtualAppliance(vmData.getName(), vmData.getStartupProcess(), 0, false, vmData.getReqDisk()));
-    }
-
-    public void createInstance(VmData vmData) {
-        instances.add(new Instance(vmData.getName(),
-                new VirtualAppliance(vmData.getName(), vmData.getStartupProcess(), 0, false, vmData.getReqDisk()),
-                new AlterableResourceConstraints(vmData.getCpu(), vmData.getCoreProcessingPower(), vmData.getRam()), vmData.getPricePerTick()));
-    }
-
     public void sendData(String data, String url) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -130,12 +73,6 @@ public class SimulatorService {
         ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
 
         //System.out.println(response.getBody());
-    }
-
-    public boolean checkDiff(VirtualMachine vm, VmData vi) {
-        return vm.getResourceAllocation().allocated.getRequiredMemory() != vi.getRam() ||
-                vm.getResourceAllocation().allocated.getRequiredCPUs() != vi.getCpu() ||
-                vm.getResourceAllocation().allocated.getRequiredProcessingPower() != vi.getCoreProcessingPower();
     }
 }
 
